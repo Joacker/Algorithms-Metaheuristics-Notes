552f75e5a25a95eb906707390bc211c58b768a71
Joacker <jfernandezvizcarra@gmail.com>
2023-10-05 14:34:13 -0300

    new commit

--- matlab_codes/VT/RS.m
+++ matlab_codes/VT/RS.m
@@ -1,79 +1,63 @@
 clear all; clc;
 
 % Parámetros que están involucrados en el método
-% - Criterio de paro (n iteraciones)
-iter = 10000
-% Definición de cota superior y cota inferior (Variables de decisión)
-lb = [-100, -100, -100, -100]
-ub = [100, 100, 100, 100]
-% Dimensiones
-dim = length(lb)
-% Llamamos a la suma de cuadrados
-F = @f_obj
-% - Temperatura se debe considerar también el como disminuir la tempera de
-% manera lineal
-T0 = 1
-% Coef alpha
-alpha = 0.99
-
-% Generar una primer Solución
-% La solución debe de estar entre el limite superior y el límite inferior
-% para cada una de las variables de decisión; se coloca el punto en la
-% multiplicación al ser de tipo vectorial para que vaya de termino a
-% termino
-X_A = lb + (ub - lb).*rand(1,dim);
-
-% Ciclo principal
+iter = 1000;
+lb = [-10, -10, -10, -10];
+ub = [10, 10, 10, 10];
+dim = length(lb);
+F = @f_obj;
+T0 = 1;
+alpha = 0.99;
+T = 1
+X_A = lb + (ub - lb).*rand(1, dim);
+bestSolution = X_A;
+bestEval = F(X_A);
+bestSolutions = zeros(iter, dim);
+% Vector para almacenar los mejores valores encontrados en cada iteración
+bestEvals = zeros(1, iter);
 for i = 1:iter
-    %Crear una segunda solución
-    X_B = X_A;
-    % Realizamos perturbación sobre X_B
-    X_B = fun_per(X_B, lb, ub, F, dim);
+    % Introducir un factor de perturbación aleatorio para aumentar la variabilidad
+    perturbation = rand(1, dim) * 2 - 1; % Vector aleatorio entre -1 y 1
+    X_B = X_A + perturbation .* (ub - lb) * 0.1; % Añadir la perturbación, ajustar el 0.1 según sea necesario
+    
+    X_B = max(min(X_B, ub), lb); % Asegurarse de que X_B no esté fuera de los límites
 
-    % Evaluar las soluciones
     eval_X_A = F(X_A);
     eval_X_B = F(X_B);
 
-    % Criterio de aceptación
-    % Indica que X_A va a ser igual a X_B o la pimera solución va a ser
-    % igual a la segunda solución, en el caso de que la segunda solución o
-    % X_B sea mejor y de no ser así o en el caso de que X_A sea mejor que
-    % X_B se va aplicar el criterio de Metropolis
     if eval_X_B < eval_X_A
         X_A = X_B;
     else
-        % Entrega u número entre 0 y 1
         r = rand();
-        P = exp( -(eval_X_B - eval_X_A) / T );
-        % Si se cumple con el criterio de evaluación o probabilidad se va a
-        % aceptar la peor solución y de no cumplirse o satisfacerse no se
-        % va a acpetar la peor solución
+        P = exp(-(eval_X_B - eval_X_A) / T);
         if r < P
-            % Se va aceptar la peor solución o se podría decir que X_A es
-            % igual a X_B, se está aceptando la peor solución, lo que
-            % permite salir de óptimos locales
             X_A = X_B;
         end
     end
-    % Ahora falta disminuir la temperatura, se amplifica por alpha siendo
-    % simpre alpha menor que 1
-    % Analicemos lo que ocurre aquí:
-    % Se divide la temperatura inicial o con la que comienza este método
-    % entre el número de iteraciones, i vendría a ser el número o contador
-    % por ciclo relacionado con el número de iteraciones
-    % Es decir a medida que van aumentando el número de iteraciones el
-    % valor generado por este término va a ir aumentando y al aumentar ese
-    % valor, el valor de la temperatura va a ir disminuyendo; es deicr, T
-    % va a ser menor. Ya que T0 se va a mantener fijo y cada vez se le está
-    % restando un valor mayor. De esta menra disminuirá de manera lineal el
-    % valor de la temperatura.
+
     T = T0 - i*(T0/iter);
-    ruta(i) = eval_X_A;
+    
+    if eval_X_A < bestEval
+        bestEval = eval_X_A;
+        bestSolution = X_A;
+    end
+    bestEvals(i) = bestEval;
+    bestSolutions(i, :) = bestSolution;
 end
 
-% Imprimimos las soluciones
-display("X_A: "+X_A)
-display("eval_X_A: "+eval_X_A)
-plot(ruta)
+display("Mejor solución: " + mat2str(bestSolution))
+display("Mejor evaluación: " + bestEval)
+for i = 1:10:iter % Modificado para imprimir cada 10 iteraciones y evitar demasiada salida
+    disp(['Iteración ', num2str(i), ': ', mat2str(bestSolutions(i, :)), ', Evaluación: ', num2str(F(bestSolutions(i, :)))]);
+end
+
+plot(1:iter, bestSolutions)
 xlabel("Iteraciones")
-ylabel("Valor en la función objetivo")
+ylabel("Mejores soluciones por componente")
+legend("x1", "x2", "x3", "x4") % Ajusta según el número de dimensiones
+figure;
+plot(bestEvals);
+xlabel('Iteraciones');
+ylabel('Mejor valor de la función objetivo encontrado');
+title('Evolución de la mejor solución');
+grid on;

d0b3247d141d76888fb36c6e98483ee280c97535
Joacker <jfernandezvizcarra@gmail.com>
2023-10-05 02:00:49 -0300

    iterations added on SVT RS

---
+++ matlab_codes/SVT_iteraciones/f_obj.m
@@ -0,0 +1,5 @@
+function eval_fun = f_obj(x)
+    % Aplicar una función de suma de cuadrados
+    % Sumar todos los cuadrados dentro de la función u objeto en este caso
+    eval_fun = sum(x.^2);
+end

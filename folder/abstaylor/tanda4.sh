5c9d1e212174040b9ed412687165bdb2c641a150
Joacker <jfernandezvizcarra@gmail.com>
2023-12-06 12:44:47 -0300

    adding some tests

--- folder/abstaylor/tanda4.sh
+++ folder/abstaylor/tanda4.sh
@@ -1,4 +1,4 @@
-files=("odfits" "hs113")
+files=("chem.bch" "chem-1.bch" "chembis.bch")
 #"ex6_2_8.bch" "ex6_1_3bis.bch" "ex6_2_12.bch" "launch.bch"
 for file in ${files[@]}
 do
@@ -6,7 +6,7 @@ do
   do
     for i in {1..5}
     do
-       ./__build__/src/ibexopt benchs/optim/with10valorless/$file --random-seed=$seed >> tanda4.log
+       ./__build__/src/ibexopt benchs/optim/hard/$file --random-seed=$seed >> tanda4.log
     done
   done
 done

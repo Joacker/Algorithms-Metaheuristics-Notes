d33ce3fd0f02f4c3394a8ab95cd7361af40e500a
Joacker <jfernandezvizcarra@gmail.com>
2023-12-06 14:44:48 -0300

    adding some tests

--- folder/hill-climbing/tanda5.sh
+++ folder/hill-climbing/tanda5.sh
@@ -1,4 +1,4 @@
-files=("ex6_1_3.bch" "sambal.bch")
+files=("himmel16.bch" "ex2_1_7.bch" "ex2_1_8.bch" "launch.bch")
 #"ex6_2_8.bch" "ex6_1_3bis.bch" "ex6_2_12.bch" "launch.bch"
 for file in ${files[@]}
 do
@@ -6,7 +6,7 @@ do
   do
     for i in {1..5}
     do
-       ./__build__/src/ibexopt benchs/optim/hard/$file --random-seed=$seed >> V1_tanda5.log
+       ./__build__/src/ibexopt benchs/optim/medium/$file --random-seed=$seed >> V1_tanda5.log
     done
   done
 done
